e6d626f432ca5105dda994f4aa8bd55e0bb292db
ZaDivan <78017590+ZaDivan@users.noreply.github.com>
2022-10-20 16:43:35 +0300

    Add files via upload

---
+++ ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductResource::Collection(Product::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated=$request->validate([
+            'name'=> 'required',
+            'description'=> 'required',
+            'price'=>'required',
+            'valid'=>'after_or_equal:now',
+        ]);
+        $product = Product::create($validated);
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product=Product::find($id);
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validated=$request->validate([
+                    'title'=> 'required',
+                    'description'=> 'required',
+                    'price'=> 'required',
+                    'valid'=> 'after_or_equal:now',
+                ]);
+                $product = Product::find($id);
+                $product->update($validated);
+                return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product=Product::find($id);
+        $product->delete();
+        return new ProductResource($product);
+    }
+}
